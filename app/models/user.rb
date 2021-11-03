6339e7ec5bfbaebabeb3eb05d487c48d6bcf8a16
khv1one <a.khvostovets@gmail.com>
2021-11-03 14:21:39 +0300

    add user pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  #attr_accessor :name, :email
+  validates(:name, presence: true, length: { maximum: 50 })
+  validates(
+    :email,
+    presence: true,
+    length: { maximum: 255 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+    uniqueness: { case_sensitive: false }
+  )
+
+  before_save { self.email = email.downcase }
+
+  has_secure_password
+  validates(:password, length: { minimum: 6 })
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw {
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  get 'signup' => 'users#new'
+  get '/signup' => 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 }
